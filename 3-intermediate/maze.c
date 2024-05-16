be7eba6698d3460c468fdd8a8d035c2d6e14b50f
Leonhard Tissen <contact@warze.org>
2024-05-16 21:02:18 +0200

    Made maze parts solid

--- 3-intermediate/maze.c
+++ 3-intermediate/maze.c
@@ -5,13 +5,7 @@
 
 // Define colors for the terminal
 #define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
+#define BWHT  "\x1B[47m"
 
 enum Tile {
     Wall,
@@ -162,9 +156,9 @@ const char* getTileTexture(int tileId) {
     switch (tileId) {
         case Wall:
         case Unpathed:
-            return "[]";
+            return (BWHT "  ");
         default:
-            return "  ";
+            return (KNRM "  ");
     }
 }
 
