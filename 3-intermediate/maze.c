8ad9883a12f39607b34ee99c6aa6ac9978edb333
Leonhard Tissen <contact@warze.org>
2024-05-17 12:02:18 +0200

    Cleanup: Singleton field to reduce total parameter count

--- 3-intermediate/maze.c
+++ 3-intermediate/maze.c
@@ -51,9 +51,10 @@ typedef struct {
     int height;
 } Field;
 
+Field field;
+
 // Function to generate a Field struct with a 2D matrix of integers
 Field generateField(int rows, int cols) {
-    Field field;
     field.width = cols * 2 + 1;
     field.height = rows * 2 + 1;
 
@@ -87,35 +88,33 @@ Field generateField(int rows, int cols) {
             }
         }
     }
-
-    return field;
 }
 
 // Function to change the tileId at a given position in a matrix
-void changeTile(int **data, Point point, int tileId) {
-    data[point.y][point.x] = tileId;
+void changeTile(Point point, int tileId) {
+    field.data[point.y][point.x] = tileId;
 }
 
 // Cuts out the border in the top left and bottom right for the start and exit
-void addStartAndExit(Field *field) {
+void addStartAndExit() {
     Point start = createPoint(1, 0);
-    changeTile(field->data, start, Solution);
-    Point finish = createPoint(field->width - 2, field->height - 1);
-    changeTile(field->data, finish, Solution);
+    changeTile(start, Solution);
+    Point finish = createPoint(field.width - 2, field.height - 1);
+    changeTile(finish, Solution);
 }
 
 // Add a wall at every 2nd tile both horizontally and vertically
-void addGridPoints(Field *field) {
-    for (int y = 0; y < field->height; y++) {
-        for (int x = 0; x < field->width; x++) {
+void addGridPoints() {
+    for (int y = 0; y < field.height; y++) {
+        for (int x = 0; x < field.width; x++) {
             if (x % 2 == 0 && y % 2 == 0) {
-                changeTile(field->data, createPoint(x, y), Wall);
+                changeTile(createPoint(x, y), Wall);
             }
         }
     }
 }
 
-int getTile(Field field, Point position) {
+int getTile(Point position) {
     if (position.x < 0 ||
         position.y < 0 ||
         position.x >= field.width ||
@@ -128,19 +127,22 @@ int getTile(Field field, Point position) {
 // Function to return the texture representation of a tileId
 const char *getTileTexture(int tileId) {
     if (tileId == Wall || tileId == Unpathed) {
+        // White block
         return (BWHT "  " KNRM);
     }
     if (show_solution && tileId == Solution) {
+        // Green block
         return (BGRE "  " KNRM);
     }
+    // Empty block
     return "  ";
 }
 
 // Function to render the field by printing its tile textures
-void renderField(Field field) {
+void renderField() {
     for (int y = 0; y < field.height; y++) {
         for (int x = 0; x < field.width; x++) {
-            int tile = getTile(field, createPoint(x, y));
+            int tile = getTile(createPoint(x, y));
             const char *tileTexture = getTileTexture(tile);
             printf("%s", tileTexture);
         }
@@ -149,18 +151,18 @@ void renderField(Field field) {
 }
 
 // Function to free the allocated memory for the Field
-void freeField(Field field) {
+void freeField() {
     for (int y = 0; y < field.height; y++) {
         free(field.data[y]);
     }
     free(field.data);
 }
 
-Point findTile(Field field, int targetTile) {
+Point findTile(int targetTile) {
     for (int y = 0; y < field.height; y++) {
         for (int x = 0; x < field.width; x++) {
             Point tile_pos = createPoint(x, y);
-            int tile = getTile(field, tile_pos);
+            int tile = getTile(tile_pos);
             if (tile == targetTile) {
                 return tile_pos;
             }
@@ -192,29 +194,29 @@ Point movePoint(Point point, int direction, int distance) {
     return new_point;
 }
 
-void moveTraverserInDirection(Field *field, int direction, int trail) {
+void moveTraverserInDirection(int direction, int trail) {
     Point new_traverser_position = movePoint(traverser, direction, 1);
     // Update the traverser position
     traverser = new_traverser_position;
 
     // Change tile to trail
-    changeTile(field->data, new_traverser_position, trail);
+    changeTile(new_traverser_position, trail);
 }
 
-void moveTraverserInOtherDirection(Field *field, int direction, int trail) {
+void moveTraverserInOtherDirection(int direction, int trail) {
     Point new_traverser_position = movePoint(traverser, direction, 1);
 
     // Change tile to trail
-    changeTile(field->data, traverser, trail);
+    changeTile(traverser, trail);
 
     // Update the traverser position
     traverser = new_traverser_position;
 }
 
-int getTileInTraverserDirection(Field field, int direction, int distance) {
+int getTileInTraverserDirection(int direction, int distance) {
     Point check_position = movePoint(traverser, direction, distance);
 
-    return getTile(field, check_position);
+    return getTile(check_position);
 }
 
 void printTraverserPosition() {
@@ -231,12 +233,12 @@ int getRandomRotationDirection() {
     return random % 2 == 0 ? 1 : -1;
 }
 
-bool canMoveTwice(Field *field, int direction) {
-    int tile = getTileInTraverserDirection(*field, direction, 1);
+bool canMoveTwice(int direction) {
+    int tile = getTileInTraverserDirection(direction, 1);
 
     if (tile != Unpathed) return false;
 
-    int tile_after = getTileInTraverserDirection(*field, direction, 2);
+    int tile_after = getTileInTraverserDirection(direction, 2);
 
     if (tile_after != Unpathed) return false;
 
@@ -244,47 +246,47 @@ bool canMoveTwice(Field *field, int direction) {
 }
 
 // Returns true if successfully moved in any direction
-bool traversalIteration(Field *field) {
+bool traversalIteration() {
     int random_direction = getRandomDirection();
     int rotation_direction = getRandomRotationDirection();
     for (int rotation = 0; rotation <= 3; rotation++) {
         int direction = ((random_direction + rotation * rotation_direction) + 4) % 4;
 
-        if (!canMoveTwice(field, direction)) continue;
+        if (!canMoveTwice(direction)) continue;
 
-        moveTraverserInDirection(field, direction, current_trail);
-        moveTraverserInDirection(field, direction, current_trail);
+        moveTraverserInDirection(direction, current_trail);
+        moveTraverserInDirection(direction, current_trail);
         return true;
     }
     return false;
 }
 
-void traversalUndo(Field *field) {
+void traversalUndo() {
     for (int direction = 0; direction <= 3; direction++) {
-        int tile = getTileInTraverserDirection(*field, direction, 1);
+        int tile = getTileInTraverserDirection(direction, 1);
 
         if (tile != Solution) continue;
 
-        moveTraverserInOtherDirection(field, direction, Branch);
-        moveTraverserInOtherDirection(field, direction, Branch);
+        moveTraverserInOtherDirection(direction, Branch);
+        moveTraverserInOtherDirection(direction, Branch);
     }
 }
 
-bool isTraverserAtExit(Field field) {
+bool isTraverserAtExit() {
     return (traverser.x == field.width - 2 &&
             traverser.y == field.height - 2);
 }
 
-void removeTraverser(Field *field) {
-    if (getTile(*field, traverser) != Solution) {
-        changeTile(field->data, traverser, Branch);
+void removeTraverser() {
+    if (getTile(traverser) != Solution) {
+        changeTile(traverser, Branch);
     }
     traverser.x = -1;
     traverser.y = -1;
 }
 
-void traverseField(Field *field) {
-    while (!isTraverserAtExit(*field)) {
+void traverseField() {
+    while (!isTraverserAtExit()) {
         bool move_successful = traversalIteration(field);
 
         if (!move_successful) {
@@ -294,18 +296,18 @@ void traverseField(Field *field) {
     removeTraverser(field);
 }
 
-void traverseFieldWithoutUndo(Field *field) {
+void traverseFieldWithoutUndo() {
     while (traversalIteration(field)) {};
 
     removeTraverser(field);
 }
 
-void lookForPotentialBranches(Field *field) {
-    for (int y = 1; y <= field->height - 2; y += 2) {
-        for (int x = 1; x <= field->width - 2; x += 2) {
+void lookForPotentialBranches() {
+    for (int y = 1; y <= field.height - 2; y += 2) {
+        for (int x = 1; x <= field.width - 2; x += 2) {
             Point check_position = createPoint(x, y);
 
-            int tile = getTile(*field, check_position);
+            int tile = getTile(check_position);
 
             if (tile != Solution && tile != Branch) continue;
 
@@ -339,32 +341,32 @@ int main() {
     show_solution = show_solution_input == 'y';
 
     // Generate the field
-    Field field = generateField(rows, cols);
+    generateField(rows, cols);
 
-    addGridPoints(&field);
+    addGridPoints();
 
     // Set the starting point
-    changeTile(field.data, createPoint(1, 1), Solution);
+    changeTile(createPoint(1, 1), Solution);
     traverser = createPoint(1, 1);
 
     // Path out a solution
-    traverseField(&field);
+    traverseField();
 
     // Branch out from the solution where possible
     current_trail = Branch;
-    lookForPotentialBranches(&field);
+    lookForPotentialBranches();
 
-    addStartAndExit(&field);
+    addStartAndExit();
 
     // Calculate the time taken
     float end_time = (float)clock() / CLOCKS_PER_SEC;
 
     // Render the field to the terminal
-    renderField(field);
+    renderField();
 
     // Print the time taken
     printf("Time taken: %f seconds\n", end_time - start_time);
 
     // Free the allocated emory for the Field
-    freeField(field);
+    freeField();
 }
